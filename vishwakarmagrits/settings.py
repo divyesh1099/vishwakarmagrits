6d77e40b6aa715d5045ab0459f0bf0984a0f4a33
Divyesh Vishwakarma <divyesh1099@gmail.com>
2021-12-26 04:10:34 +0530

    Heroku Final Deploy

--- vishwakarmagrits/settings.py
+++ vishwakarmagrits/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t2%!81^2qxsd3@ypnmsyq6!=(cnba^*0xg)1wfmt45k6hh5f%g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'vishwakarmagrits.com', 'vishwakarmagrits.in']
+ALLOWED_HOSTS = ['127.0.0.1', 'vishwakarmagrits.com', 'vishwakarmagrits.in', 'url', 'localhost']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
